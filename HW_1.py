ab70194e226606851d70eaa936d510e1fd538d5e
Tetiana <athlantadaniella@gmail.com>
2025-12-30 00:50:54 +0100

    Home task 1

---
+++ HW_1.py
@@ -0,0 +1,56 @@
+# #Task_1:
+# x=int(input('Enter number: '))
+
+# print(x ** 2)
+
+
+# #Task_2:
+# x=int(input('Enter number 1: '))
+# y=int(input('Enter number 2: '))
+# z=int(input('Enter number 2: '))
+# avg = (x + y + z) / 3
+
+# print(avg)
+
+
+# #Task_3:
+# x = int(input('Enter minutes: '))
+# y = x // 60 #hours
+# z = x % 60 #remaining minutes
+
+# print(f"{y} hours {z} minutes")
+
+
+# #Task_4:
+# x = float(input('Enter price: '))
+# y = float(input('Enter discount, %: '))
+# final_price = x * (100 - y) / 100
+#
+# print(final_price)
+
+
+# # Task_5:
+# x = int(input('Enter number: '))
+# y = int(x) % 10 #last digit
+#
+# print(f"Last digit: {y}")
+
+
+# # Task_6:
+# x=float(input('Enter length: '))
+# y=float(input('Enter width: '))
+#
+# print(float(2*(x + y)))
+
+
+# # Task_7:
+# x=int(input('Enter 4 digit number: '))
+# thousands = x // 1000
+# hundreds = (x // 100) % 10
+# tens = (x // 10) % 10
+# units = x % 10
+#
+# print(thousands)
+# print(hundreds)
+# print(tens)
+# print(units)
