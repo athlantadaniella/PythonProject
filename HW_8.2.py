ab0f6d54f3baf8c355fea715d5572e035b77ddf5
Tetiana <athlantadaniella@gmail.com>
2026-02-15 15:59:58 +0100

    Home task 8.2

---
+++ HW_8.2.py
@@ -0,0 +1,19 @@
+def is_palindrome(text):
+    y = text.lower()
+    cleaned = ""
+    for char in y:
+        if char.isalnum():
+            cleaned += char
+    if cleaned == cleaned[::-1]:
+        return True
+    else:
+        return False
+
+text = input("Enter a text: ")
+print(is_palindrome(text))
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
