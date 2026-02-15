7de124fff03ea5d5f9f66c24072e0eec2eac78ae
Tetiana <athlantadaniella@gmail.com>
2026-02-15 16:26:58 +0100

    Home task 8.2

--- HW_8.2.py
+++ HW_8.2.py
@@ -1,14 +1,21 @@
-def find_unique_value(numbers):
+def is_palindrome(text):
+    y = text.lower()
+    cleaned = ""
+    for char in y:
+        if char.isalnum():
+            cleaned += char
 
-    for num in numbers:
-        if numbers.count(num) == 1:
-             return int(num) if num.is_integer() else num
+    if cleaned == cleaned[::-1]:
+        return True
+    else:
+        return False
 
-numbers = input("Enter some numbers: ").replace(',', ' ').split()
-numbers = [float(x) for x in numbers]
-print(find_unique_value(numbers))
+text = input("Enter a text: ")
+print(is_palindrome(text))
 
-assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
-assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
-assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
 print("ОК")
