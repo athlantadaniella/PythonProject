cab65a5d8556392fe9f98e735469009b26643316
Tetiana <athlantadaniella@gmail.com>
2026-02-22 22:53:06 +0100

    Home task 11.1

---
+++ HW_11.1.py
@@ -0,0 +1,23 @@
+from inspect import isgenerator
+
+def prime_generator(end):
+    for number in range(2, end + 1):
+        is_prime = True
+
+        for divisor in range(2, number):
+            if number % divisor == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            yield number
+
+print(list(prime_generator(29)))
+
+gen = prime_generator(1)
+
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
